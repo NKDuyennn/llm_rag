cc79f216cde65ff94a24d1904915e03809cacb60
Nguyễn Kiều Duyên <127535688+NKDuyennn@users.noreply.github.com>
2024-06-20 01:45:08 +0700

    Update streamlit_app_mamba.py

--- src/streamlit_app_mamba.py
+++ src/streamlit_app_mamba.py
@@ -198,5 +198,5 @@ if sidebar_selection == "Vision Mamba":
                 st.write(message.content)
 
 elif sidebar_selection == "AI-Therapist":
-    st.markdown('<meta http-equiv="refresh" content="0; url=https://huggingface.co/datasets/fadodr/mental_health_dataset">', unsafe_allow_html=True)
+    st.markdown('<meta http-equiv="refresh" content="0; url=https://share.streamlit.io/app/nkduyen-chattherapist/">', unsafe_allow_html=True)
 
