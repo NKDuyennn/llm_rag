6dc7a5da396a9e3c4132a874a33fbe4c9e73fc64
Nguyễn Kiều Duyên <127535688+NKDuyennn@users.noreply.github.com>
2024-06-20 02:04:31 +0700

    Update streamlit_app_therapist.py

--- src/streamlit_app_therapist.py
+++ src/streamlit_app_therapist.py
@@ -196,4 +196,4 @@ if sidebar_selection == "AI-Therapist":
             with st.chat_message("Human"):
                 st.write(message.content)
 elif sidebar_selection == "Vision Mamba":
-    st.markdown('<meta http-equiv="refresh" content="0; url=https://nkduyen-mamba.streamlit.app/">', unsafe_allow_html=True)
+    st.markdown('<a href="https://nkduyen-mamba.streamlit.app/" target="_blank">Click here to access Q&A about Mamba</a>', unsafe_allow_html=True)
